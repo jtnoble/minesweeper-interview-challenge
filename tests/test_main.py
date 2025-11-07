2d77533f05c27db8422d9e0cf0959ac5fd412587
jtnoble <jtnoble321@gmail.com>
2025-11-06 20:52:18 -0600

    Typehinting, Comment additions, Matrix Fixture

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,22 +2,16 @@ import pytest
 from py_check.main import minesweeper, get_neighbors, count_neighbor_mines
 from textwrap import dedent
 
-"""
-
-*...
-....
-.*..
-....
-
-
-*100
-2210
-1*10
-1110
-
-"""
-
+@pytest.fixture()
+def matrix() -> list[list]:
+    return [
+        ['.', '.', '.', '.'],
+        ['.', '*', '*', '.'],
+        ['.', '.', '.', '.'],
+    ]
+    
 def test_minesweeper() -> None:
+    """Check that the output of minesweeper replaces the '.'s with numbers equal to their adjacent mines"""
     example = dedent('''
         *...
         ....
@@ -36,7 +30,7 @@ def test_minesweeper() -> None:
 
     assert result == goal
 
-def test_get_neighbors():
+def test_get_neighbors() -> None:
     matrix = [
         ['.', '.', '.', '.'],
         ['.', '.', '*', '.'],
@@ -46,42 +40,26 @@ def test_get_neighbors():
     expected = '*.......'
     assert result == expected
 
-def test_get_neighbors_top_left():
-    matrix = [
-        ['.', '.', '.', '.'],
-        ['.', '*', '*', '.'],
-        ['.', '.', '.', '.'],
-    ]
+def test_get_neighbors_top_left(matrix: list[list]) -> None:
+    """Check the top left of the matrix"""
     result = get_neighbors(matrix, 0, 0)
     expected = '..*'
     assert result == expected
 
-def test_get_neighbors_top_right():
-    matrix = [
-        ['.', '.', '.', '.'],
-        ['.', '*', '*', '.'],
-        ['.', '.', '.', '.'],
-    ]
+def test_get_neighbors_top_right(matrix: list[list]) -> None:
+    """Check the top right of the matrix"""
     result = get_neighbors(matrix, 0, 3)
     expected = '..*'
     assert result == expected
 
-def test_get_neighbors_bottom_left():
-    matrix = [
-        ['.', '.', '.', '.'],
-        ['.', '*', '*', '.'],
-        ['.', '.', '.', '.'],
-    ]
+def test_get_neighbors_bottom_left(matrix: list[list]) -> None:
+    """Check the bottom left of the matrix"""
     result = get_neighbors(matrix, 2, 0)
     expected = '..*'
     assert result == expected
 
-def test_get_neighbors_bottom_right():
-    matrix = [
-        ['.', '.', '.', '.'],
-        ['.', '*', '*', '.'],
-        ['.', '.', '.', '.'],
-    ]
+def test_get_neighbors_bottom_right(matrix: list[list]) -> None:
+    """Check the bottom right of the matrix"""
     result = get_neighbors(matrix, 2, 3)
     expected = '..*'
     assert result == expected
@@ -92,8 +70,5 @@ def test_get_neighbors_bottom_right():
 ))
 def test_count_neighbor_mines(line: str, count: int) -> None:
     """Check that amount of *s is counted correctly"""
-
     result = count_neighbor_mines(line)
     assert result == count
-
-
