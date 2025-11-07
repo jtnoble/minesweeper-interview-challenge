9a67185bd9fcd829ba23cfa45bd8620f0f66be30
jtnoble <jtnoble321@gmail.com>
2025-11-06 20:53:27 -0600

    Additional comments, swapped string addition with joined lists, small optimizations

--- py_check/main.py
+++ py_check/main.py
@@ -1,20 +1,18 @@
-from dataclasses import dataclass
-
-# "....\n..*."
-
-# ['.', '.' ...], ['.', '.']
-
 def minesweeper(sweep_text: str) -> str:
+    """
+    Input: string input of minesweeper board
+        example: '*...\n....\n.*..\n....'
+    Output: string output of board with number of mines next to a space. 
+        example: '*100\n2210\n1*10\n1110'
+    """
     result = ""
-    separate_lines = sweep_text.splitlines()
-    # current ['*...', '....', '.*..', '....']
+    separate_lines = sweep_text.splitlines() # ['*...', '....', '.*..', '....']
 
-    matrix = []
-    for line in separate_lines:
-        matrix.append(list(line))
-    
-    # new ['*...']
-    #     ['....']
+    matrix = [line for line in separate_lines]
+    # matrix = ['*...']
+    #          ['....']
+    #          ['.*..']
+    #          ['....']
 
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
@@ -29,47 +27,50 @@ def minesweeper(sweep_text: str) -> str:
 
 
 def get_neighbors(matrix: list[list], row: int, col: int) -> str:
-    values = ""
+    """Given a matrix, row, and column, return a string of only row,col coords and its surrounding neighbors"""
+    values = []
     
     # right
     if col + 1 < len(matrix[row]):
-        values += matrix[row][col+1]
+        values.append(matrix[row][col+1])
     
     # left
     if col - 1 >= 0:
-        values += matrix[row][col-1]
+        values.append(matrix[row][col-1])
 
     # up
     if row + 1 < len(matrix):
-        values += matrix[row+1][col]
+        values.append(matrix[row+1][col])
 
     # down right
     if col + 1 < len(matrix[row]) and row + 1 < len(matrix):
-        values += matrix[row+1][col+1]
+        values.append(matrix[row+1][col+1])
 
     # down left
     if col - 1 >= 0 and row + 1 < len(matrix):
-        values += matrix[row+1][col-1]
+        values.append(matrix[row+1][col-1])
 
     # up
     if row - 1 >= 0:
-        values += matrix[row-1][col]
+        values.append(matrix[row-1][col])
     
     # up right
     if row - 1 >= 0 and col + 1 < len(matrix[row]):
-        values += matrix[row-1][col+1]
+        values.append(matrix[row-1][col+1])
 
     # up left
     if row - 1 >= 0 and col - 1 >= 0:
-        values += matrix[row-1][col-1]
+        values.append(matrix[row-1][col-1])
 
-    return values
+    return "".join(values)
 
 
 def count_neighbor_mines(line: str) -> int:
+    """Count the amount of mines in a string"""
     return line.count('*')
 
 if __name__ == "__main__":
-    print("\n\nHello World!\n\n")
+    test_input = '*...\n....\n.*..\n....'
+    output = minesweeper(test_input)
+    print(f'Input:\n{test_input}\n\nOutput:\n{output}')
 
-    minesweeper()
